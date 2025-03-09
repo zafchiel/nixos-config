f96fbcef029e5aae6eab49e22310cc881d2fda0e
zafchiel <bialakropla@proton.me>
2025-03-09 21:35:41 +0100

    test zsh-autocomplete

--- home.nix
+++ home.nix
@@ -38,6 +38,15 @@
 				"history"
 				"sudo"
 				"command-not-found"
+				{
+        name = "zsh-autocomplete"; # completes history, commands, etc.
+        src = pkgs.fetchFromGitHub {
+          owner = "marlonrichert";
+          repo = "zsh-autocomplete";
+          rev = "762afacbf227ecd173e899d10a28a478b4c84a3f";
+          sha256 = "1357hygrjwj5vd4cjdvxzrx967f1d2dbqm2rskbz5z1q6jri1hm3";
+        };
+      }
 			];
 			theme = "robbyrussell";
 		};
