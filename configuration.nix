50b483a88109251e4cff0c14a70953d8890b9c0e
zafchiel <bialakropla@proton.me>
2025-03-10 21:29:49 +0100

    add vitals gnome extension

--- configuration.nix
+++ configuration.nix
@@ -106,6 +106,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
     home-manager
+    gnomeExtensions.vitals
   ];
 
   # Automatic updates
