5d7849c4cd9213e263014306e2da7ef058eaee14
zafchiel <bialakropla@proton.me>
2025-03-12 15:42:43 +0100

    comment out not used line

--- configuration.nix
+++ configuration.nix
@@ -78,7 +78,7 @@
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
-  services.xserver.libinput.enable = true;
+  # services.xserver.libinput.enable = true;
 
   # Enable zsh
   programs.zsh.enable = true;
