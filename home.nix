51562542516af8117012cba52ebbd0a6cd3d1a9a
zafchiel <bialakropla@proton.me>
2025-03-17 08:52:52 +0100

    remove zsh config from nix

--- home.nix
+++ home.nix
@@ -1,6 +1,11 @@
 { config, pkgs, ... }: {
   nixpkgs.config.allowUnfree = true;
 
+  imports = [
+    ./zsh/zsh.nix
+    ./cliphist.nix
+  ];
+
 	home = {
 		username = "zafchiel";
 		homeDirectory = "/home/zafchiel";
@@ -15,6 +20,7 @@
 			lazygit
 			ungoogled-chromium
 			obsidian
+      eza
 
 			wget
 			ripgrep
@@ -29,41 +35,6 @@
 	};
 
 	programs.home-manager.enable = true;
-
-	programs.zsh = {
-		enable = true;
-		autosuggestion.enable = true;
-    # enableCompletion = true;
-    # syntaxHighlighting.enable = true;
-
-		plugins = [
-			{
-				name = "zsh-autocomplete"; # completes history, commands, etc.
-				src = pkgs.fetchFromGitHub {
-				  owner = "marlonrichert";
-				  repo = "zsh-autocomplete";
-				  rev = "762afacbf227ecd173e899d10a28a478b4c84a3f";
-				  sha256 = "1357hygrjwj5vd4cjdvxzrx967f1d2dbqm2rskbz5z1q6jri1hm3";
-				};
-		  }
-		];
-		
-		oh-my-zsh = {
-			enable = true;
-			plugins = [
-				"git"
-			];
-			theme = "robbyrussell";
-		};
-		
-		shellAliases = {
-			la = "ls -A";
-			lg = "lazygit";
-			vim = "nvim";
-			v = "nvim";
-			pn = "pnpm";
-		};
-	};
 
 	programs.git = {
 		enable = true;
